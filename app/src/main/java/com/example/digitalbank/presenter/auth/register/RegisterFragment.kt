2b93dd5ca6da2201eb99de8019aaf8c837618cda
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-06-21 23:54:23 -0300

    04 Criando layout register

--- app/src/main/java/com/example/digitalbank/presenter/auth/register/RegisterFragment.kt
+++ app/src/main/java/com/example/digitalbank/presenter/auth/register/RegisterFragment.kt
@@ -5,15 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.digitalbank.R
+import com.example.digitalbank.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
