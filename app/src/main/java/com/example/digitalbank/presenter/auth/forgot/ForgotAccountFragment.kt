b439d63ddd287053e8b8a8adb18fa06b43106dc3
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-06-21 23:43:52 -0300

    03 Criando layout recover

--- app/src/main/java/com/example/digitalbank/presenter/auth/forgot/ForgotAccountFragment.kt
+++ app/src/main/java/com/example/digitalbank/presenter/auth/forgot/ForgotAccountFragment.kt
@@ -5,15 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.digitalbank.R
+import com.example.digitalbank.databinding.FragmentForgotAccountBinding
 
 class ForgotAccountFragment : Fragment() {
+
+    private var _binding: FragmentForgotAccountBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot_account, container, false)
+    ): View {
+        _binding = FragmentForgotAccountBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
