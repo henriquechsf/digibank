43938ecda6643ae5c3cb1900921140bfca1d5053
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-06-22 23:10:34 -0300

    24 criando login view model

---
+++ app/src/main/java/com/example/digitalbank/presenter/auth/login/LoginViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.digitalbank.presenter.auth.login
+
+import androidx.lifecycle.liveData
+import com.example.digitalbank.domain.auth.LoginUseCase
+import com.example.digitalbank.utils.StateView
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val loginUseCase: LoginUseCase
+) {
+
+    fun login(email: String, password: String) = liveData(Dispatchers.IO) {
+        try {
+            emit(StateView.Loading())
+
+            loginUseCase.invoke(email, password)
+
+            emit(StateView.Sucess(Unit))
+        } catch (e: Exception) {
+            emit(StateView.Error(e.message))
+        }
+    }
+}
