11630248cb3ea6488e08c75afa5b75fac5c2f80d
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-06-22 23:12:08 -0300

    25 criando forgot view model

---
+++ app/src/main/java/com/example/digitalbank/presenter/auth/forgot/ForgotViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.digitalbank.presenter.auth.forgot
+
+import androidx.lifecycle.liveData
+import com.example.digitalbank.domain.auth.ForgotUseCase
+import com.example.digitalbank.utils.StateView
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+@HiltViewModel
+class ForgotViewModel @Inject constructor(
+    private val forgotUseCase: ForgotUseCase
+) {
+
+    fun forgot(email: String) = liveData(Dispatchers.IO) {
+        try {
+            emit(StateView.Loading())
+
+            forgotUseCase.invoke(email)
+
+            emit(StateView.Sucess(Unit))
+        } catch (e: Exception) {
+            emit(StateView.Error(e.message))
+        }
+    }
+}
