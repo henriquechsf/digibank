4f5589a8734e73acfaaeb5de0cecfe313db59b21
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-06-23 23:12:44 -0300

    30 recuperando senha da conta

--- app/src/main/java/com/example/digitalbank/presenter/auth/forgot/ForgotAccountFragment.kt
+++ app/src/main/java/com/example/digitalbank/presenter/auth/forgot/ForgotAccountFragment.kt
@@ -5,8 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.digitalbank.R
 import com.example.digitalbank.databinding.FragmentForgotAccountBinding
+import com.example.digitalbank.utils.StateView
 import com.example.digitalbank.utils.initToolbar
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,6 +20,8 @@ class ForgotAccountFragment : Fragment() {
 
     private var _binding: FragmentForgotAccountBinding? = null
     private val binding get() = _binding!!
+
+    private val forgotViewModel: ForgotViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,7 +40,7 @@ class ForgotAccountFragment : Fragment() {
 
     private fun initListeners() {
         binding.btnSendMail.setOnClickListener {
-
+            validateData()
         }
     }
 
@@ -42,9 +49,39 @@ class ForgotAccountFragment : Fragment() {
         val email = edtEmail.text.toString().trim()
 
         if (email.isNotEmpty()) {
-
+            forgotAccount(email)
         } else {
             Toast.makeText(requireContext(), "Digite todos os campos", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun forgotAccount(email: String) {
+        forgotViewModel.forgot(email).observe(viewLifecycleOwner) { stateView ->
+            when (stateView) {
+                is StateView.Loading -> {
+                    binding.progressBar.isVisible = true
+                }
+                is StateView.Sucess -> {
+                    binding.progressBar.isVisible = false
+
+                    findNavController().popBackStack()
+
+                    Toast.makeText(
+                        requireContext(),
+                        "Email enviado com sucesso.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                is StateView.Error -> {
+                    binding.progressBar.isVisible = false
+
+                    Toast.makeText(
+                        requireContext(),
+                        stateView.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
         }
     }
 
