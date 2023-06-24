a7265808db3212f635dd23c2370c97dafd77e611
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-06-23 22:37:32 -0300

    27 realizando ajuste do fluxo de autenticacao

--- app/src/main/java/com/example/digitalbank/presenter/home/HomeFragment.kt
+++ app/src/main/java/com/example/digitalbank/presenter/home/HomeFragment.kt
@@ -5,15 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.digitalbank.R
+import com.example.digitalbank.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
