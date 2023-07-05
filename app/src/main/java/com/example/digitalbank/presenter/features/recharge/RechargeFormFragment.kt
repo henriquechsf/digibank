a1ede06f75e31dc526527974eb33a008abe0e4f5
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-07-04 21:25:34 -0300

    60 layout form recarga

---
+++ app/src/main/java/com/example/digitalbank/presenter/features/recharge/RechargeFormFragment.kt
@@ -0,0 +1,27 @@
+package com.example.digitalbank.presenter.features.recharge
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.digitalbank.databinding.FragmentRechargeFormBinding
+
+class RechargeFormFragment : Fragment() {
+
+    private var _binding: FragmentRechargeFormBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRechargeFormBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
