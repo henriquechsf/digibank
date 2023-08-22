df02325fa5ff39d50c225a051139b441ed2a1aa5
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-08-22 12:12:37 -0300

    65 - adapter listagem de usuarios

--- app/src/main/java/com/example/digitalbank/data/model/User.kt
+++ app/src/main/java/com/example/digitalbank/data/model/User.kt
@@ -7,5 +7,7 @@ data class User(
     var name: String = "",
     var email: String = "",
     var phone: String = "",
+    var image: String = "",
+    @get:Exclude
     var password: String = ""
 )
---
+++ app/src/main/java/com/example/digitalbank/presenter/features/transfer/adapter/TransferUserAdapter.kt
@@ -0,0 +1,62 @@
+package com.example.digitalbank.presenter.features.transfer.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.digitalbank.data.model.User
+import com.example.digitalbank.databinding.TransferUserItemBinding
+
+class TransferUserAdapter(
+    private val userSelected: (User) -> Unit
+) : ListAdapter<User, TransferUserAdapter.ViewHolder>(DIFF_CALLBACK) {
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<User>() {
+            override fun areItemsTheSame(
+                oldItem: User,
+                newItem: User
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: User,
+                newItem: User
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            TransferUserItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val user = getItem(position)
+
+        with(holder.binding) {
+            tvUsername.text = user.name
+            Glide.with(root.context)
+                .load(user.image)
+                .into(imgUser)
+        }
+
+        holder.itemView.setOnClickListener {
+            userSelected(user)
+        }
+    }
+
+    inner class ViewHolder(val binding: TransferUserItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+}
